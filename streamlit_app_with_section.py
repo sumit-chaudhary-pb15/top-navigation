eb3ad7dd3bff2f338d4918c6afeb869ef2d35e2f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-06-21 00:01:34 +0700

    Update streamlit_app_with_section.py

--- streamlit_app_with_section.py
+++ streamlit_app_with_section.py
@@ -121,11 +121,10 @@ about = st.Page(about_page, title="About", icon=":material/info:")
 contact = st.Page(contact_page, title="Contact", icon=":material/mail:")
 
 # Set up top navigation with sections
-pages = {
+current_page = st.navigation(
     "Main": [home, dashboard],
-    "Information": [about, contact]
-    }
-current_page = st.navigation(pages, position="top")
+    "Information": [about, contact], 
+    position="top")
 
 # Run the selected page
 current_page.run()
