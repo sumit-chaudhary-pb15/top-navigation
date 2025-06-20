18b4c6e74ce96483061fee0effa7fefd6d71c122
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-06-20 23:43:14 +0700

    Update streamlit_app_with_section.py

--- streamlit_app_with_section.py
+++ streamlit_app_with_section.py
@@ -124,7 +124,7 @@ contact = st.Page(contact_page, title="Contact", icon=":material/mail:")
 current_page = st.navigation(
     {
         # "Main": [home, dashboard],
-        [home, dashboard],
+        home, dashboard,
         "Information": [about, contact]
     },
     position="top"
